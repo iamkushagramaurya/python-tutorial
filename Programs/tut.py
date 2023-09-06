7e4fcb648938edeeafe18b6666c539c7e35d2f66
Kushagra Maurya <iamkushagramaurya@gmail.com>
2023-09-06 23:01:46 +0530

    first tut

---
+++ Programs/tut.py
@@ -0,0 +1,11 @@
+# Variable Declaration in java
+
+# String str ="" ;
+
+f = 57.9 # float  
+str = "Shruti"  # String
+l = list()  #List
+l1 = []  #List
+tup = tuple()
+set = set()
+dic = dict() # you can say it json format
